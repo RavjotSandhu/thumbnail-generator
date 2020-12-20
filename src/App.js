b93e1851830710a94d0923e44b0905283034e654
RavjotSandhu <nand2072@gmail.com>
2020-12-20 18:30:42 +0530

    added app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+ state = {
+   screenshot:null
+ }
+
+
+  makeThumbnail = (e) => {
+    e.preventDefault();
+    const link = e.target.elements.link.value;
+    axios.get(`https://screenshotapi.net/api/v1/screenshot?url=${link}&token=8RAK9GNCPR3ZLOQK1K5C8LISRDBI39GU`)
+    .then((res) => {
+      const screenshot = res.data.screenshot;
+      this.setState({screenshot})
+      console.log(screenshot);
+    });
+  }
+  render() {
+    return (
+      <div>
+        <form className="site" onSubmit={this.makeThumbnail}>
+        <input type="text" name="link" placeholder="Enter your Url here..."/>
+        <button type="submit">Submit</button>
+        </form>
+        { this.state.screenshot ? <img src={ this.state.screenshot } alt="link"/> : <p>Please enter your link.</p> }
+      </div>
+    )
+  }
 }
-
-export default App;
